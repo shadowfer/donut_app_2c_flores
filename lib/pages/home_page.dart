657656a43f15ccd7a0c7bded904eff85dd21fdb0
shadowfer <fernely24k@gmail.com>
2025-02-12 13:29:37 -0600

    Main Text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,13 +11,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        //icono izquierdo
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
-        actions: [Icon(Icons.person)],
-      ),
-      //Icono derecho
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          //icono izquierdo
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    'I want to ',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text('Eat',
+                      style: TextStyle(
+                          //Tamaño de letras
+                          fontSize: 32,
+                          //Negritas
+                          fontWeight: FontWeight.bold,
+                          //Subrayado
+                          decoration: TextDecoration.underline))
+                ],
+              ),
+            ),
+
+            //Pestañas(TapBar)
+
+            //Contenido de pestañas(TapBarView)
+
+            //Carrito(Car)
+          ],
+        )
+        //Icono derecho
+        );
   }
 }
