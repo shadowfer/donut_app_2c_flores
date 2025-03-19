d0c96ba7242e08030a288ff2610462e969e19370
shadowfer <fernely24k@gmail.com>
2025-03-19 13:11:46 -0600

    UI FINISHED

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,61 +27,109 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            //icono izquierdo
-            leading: Icon(Icons.menu, color: Colors.grey[800]),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 24.0),
-                child: Icon(Icons.person),
-              )
-            ],
-          ),
-          body: Column(
-            children: [
-              //Texto principal
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-                child: Row(
-                  children: [
-                    Text(
-                      'I want to ',
-                      style: TextStyle(fontSize: 32),
-                    ),
-                    Text('Eat',
-                        style: TextStyle(
-                            //Tamaño de letras
-                            fontSize: 32,
-                            //Negritas
-                            fontWeight: FontWeight.bold,
-                            //Subrayado
-                            decoration: TextDecoration.underline))
-                  ],
-                ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          //icono izquierdo
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    'I want to ',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text('Eat',
+                      style: TextStyle(
+                          //Tamaño de letras
+                          fontSize: 32,
+                          //Negritas
+                          fontWeight: FontWeight.bold,
+                          //Subrayado
+                          decoration: TextDecoration.underline))
+                ],
               ),
+            ),
 
-              //Pestañas(TabBar)
-              TabBar(tabs: myTabs),
-              //Contenido de pestañas(TabBarView)
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    DonutTab(),
-                    BurgerTab(),
-                    SmoothieTab(),
-                    PancakesTab(),
-                    PizzaTab(),
-                    //Aquí se agregarán las pestañas de los demás productos.
-                  ],
-                ),
-              )
-              //Carrito(Car)
-            ],
-          )
-          //Icono derecho
-          ),
+            //Pestañas(TabBar)
+            TabBar(tabs: myTabs),
+            //Contenido de pestañas(TabBarView)
+            Expanded(
+              child: TabBarView(
+                children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothieTab(),
+                  PancakesTab(),
+                  PizzaTab(),
+                  //Aquí se agregarán las pestañas de los demás productos.
+                ],
+              ),
+            ),
+            //Cart Section
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(16),
+              child: Row(
+                //Esto alinea los elementos a los extremos
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      //Alinear horizontalmente una columna
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "2 Items | \$45",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text("Delivery Charges Included"),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.pink,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24, vertical: 12)),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "View Cart",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
